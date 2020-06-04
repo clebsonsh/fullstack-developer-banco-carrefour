2bde33f00c898a1e63501eeddfdd0018b800c791
Clebson Moura <clebson.sh@gmail.com>
2020-06-04 00:17:50 -0300

    C# exemple 1 done

---
+++ EstruturaDePrograma/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using EstruturaDePrograma.Exemplos;
+
+namespace EstruturaDePrograma
+{
+    class Program
+    {
+        static void Main()
+        {
+            var pilha = new Pilha();
+            pilha.Empilha(1);
+            pilha.Empilha(10);
+            pilha.Empilha(100);
+
+            Console.WriteLine(pilha.Desempilha());
+            Console.WriteLine(pilha.Desempilha());
+            Console.WriteLine(pilha.Desempilha());
+            Console.WriteLine(pilha.Desempilha());
+        }
+    }
+}
